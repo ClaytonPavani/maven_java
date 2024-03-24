b7e94167835727f8e45507e1d230afdad52a5dab
ClaytonPavani <claytonpavani@gmail.com>
2024-03-24 14:27:51 -0300

    Commit com projeto utilizando maven

---
+++ src/test/java/ProdutoTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProdutoTest {
+
+    @Test
+    public void verificarJunit(){
+        Assertions.assertTrue(true);
+    }
+
+}
